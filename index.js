a7e7a27fe8930ae4ddc190a8a402caba7ea7089c
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2022-07-29 11:38:43 +0100

    add database

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
 require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000 
+})
+
+const connection = mongoose.connection
+connection.on('error', console.error.bind(console, 'connection error:'))
+connection.once('open', () => {
+  console.log("MongoDb Database connection established successfully")
+})
 
 app.use(cors())
 app.use(express.static('public'))
